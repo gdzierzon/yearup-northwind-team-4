c3b3286373c340dd98c5ab1bcadacf2ffd34c62e
Teala-B <bradleyteala@gmail.com>
2023-12-08 17:27:31 -0500

    added the view all functionality and displaying all products and details

--- northwind-traders-website/scripts/products.js
+++ northwind-traders-website/scripts/products.js
@@ -15,10 +15,19 @@ async function filterProducts(){
     if(selectValue === "View All"){
         products = await productsService.getAllProducts()
         console.log(products);
-        displayProducts(products)
+        products.forEach(displayProduct)
     }
 }
 
-function displayProducts(products) {
-    
+function displayProduct(product) {
+      // clone template html instead of building the card by hand
+      const card = productsTemplate.content.cloneNode(true)
+
+      // set all values
+      card.getElementById("products-header").innerText = product.productName
+      card.getElementById("supplier-value").innerText = product.supplier
+      card.getElementById("stock-value").innerText = product.unitsInStock
+      card.getElementById("price-value").innerText = product.unitPrice
+  
+      productContainer.appendChild(card)
 }
