8a7df09e83532136032c36b9a7cc436ff6ada67e
Teala-B <bradleyteala@gmail.com>
2023-12-08 16:58:08 -0500

    added alogic for view all

--- northwind-traders-website/scripts/products.js
+++ northwind-traders-website/scripts/products.js
@@ -1,14 +1,24 @@
 let productsService;
 let selectProducts;
+let products;
 document.addEventListener("DOMContentLoaded",()=>{
 selectProducts=document.getElementById("selectProducts");
 selectProducts.addEventListener("change",filterProducts)
 })
 
-function filterProducts(){
+async function filterProducts(){
     selectProducts=document.getElementById("selectProducts");
     let selectValue=selectProducts.value;
     console.log(selectValue);
     
+    productsService = new ProductsService()
+    if(selectValue === "View All"){
+        products = await productsService.getAllProducts()
+        console.log(products);
+        displayProducts(products)
+    }
+}
 
+function displayProducts(products) {
+    
 }
